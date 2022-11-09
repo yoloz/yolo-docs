b037264f344150235480362727a95849d494daeb
yoloz <youloz@outlook.com>
2022-11-09 11:22:49 +0800

    移除其他语言

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -24,8 +24,8 @@ const config = {
   // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'zh-CN',
-
-    locales: ['zh-CN', 'en', 'fr', 'pt-BR', 'ko'],
+    locales: ['zh-CN'],
+    // ['en', 'fr', 'pt-BR', 'ko'],
   },
 
   presets: [
@@ -37,15 +37,13 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/yoloz/yolo-docs/tree/docusaurus/',
+          editUrl: 'https://github.com/yoloz/yolo-docs/tree/docusaurus/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/yoloz/yolo-docs/tree/docusaurus/',
+          editUrl: 'https://github.com/yoloz/yolo-docs/tree/docusaurus/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -151,7 +149,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['java','scala','log','properties'],
+        additionalLanguages: ['java', 'scala', 'log', 'properties'],
       },
     }),
 }
